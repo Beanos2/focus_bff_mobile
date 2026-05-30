2eb671229451e42e3913598a91f02bbbe539ac78
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-05-29 20:47:07 -0400

    added proxy methods to service and endpoints in a new controller

---
+++ app/api/v1/auth_controller.py
@@ -0,0 +1,29 @@
+import httpx
+from litestar import Controller, post
+from litestar.exceptions import HTTPException
+
+
+from app.clients.auth_client import proxy_register, proxy_login
+
+class AuthBFFController(Controller):
+    path = "/api/v1/auth"
+
+    @post("/register")
+    async def register(self, data: dict) -> dict:
+        try:
+            return await proxy_register(data)
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                detail="Error en el registro (¿Correo duplicado?)", 
+                status_code=e.response.status_code
+            )
+        
+    @post("/login")
+    async def login(self, data: dict) -> dict:
+        try:
+            return await proxy_login(data)
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                detail="Credenciales inválidas", 
+                status_code=e.response.status_code
+            )
